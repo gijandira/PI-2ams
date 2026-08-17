eb798909a4211a88dcf6842b062818e1ba453cfc
Vitor Silva <vitorsilvagouvea@gmail.com>
2026-08-17 17:27:36 -0300

    Delete autimm directory

--- autimm/backend/routes/comunicacaoroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const comunicacaoController = require('../controllers/comunicacaocontroller');
-
-router.get('/categorias', comunicacaoController.getCategorias);
-router.get('/cards', comunicacaoController.getCards);
-
-module.exports = router;
